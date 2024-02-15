897c046a0100bf6c7b775d9ff1bb513d26b3ddc0
klein75 <kleindbb21@gmail.com>
2024-02-15 00:26:06 -0500

    se agrego comentario

---
+++ index/src/main/java/prueProyec1/index/IndexApplication.java
@@ -0,0 +1,25 @@
+package prueProyec1.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class IndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexApplication.class, args);
+	}
+	@GetMapping("/hola")
+    public String hola(@RequestParam(value = "name", defaultValue = "mundo") String name) {
+      return String.format("hola %s!", name);
+    }
+	@GetMapping("/adios")
+    public String adios(@RequestParam(value = "name", defaultValue = "mundo cruel") String name) {
+      return String.format("adios %s!", name);
+    }
+}
+
